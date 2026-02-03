74e4100246a50c5f141ed29d1c08f67fc0c6e8cc
shital360 <shitaldangol@gmail.com>
2026-02-03 17:40:34 +0545

    initial commit

---
+++ src/middleware/auth.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  const decoded = jwt.verify(token, "secret");
+  req.user = decoded;
+  next();
+};
